1bf4786a8f26d9f616470c4f6dc271eeafa12ff1
victor49 <vl491249@gmail.com>
2026-01-25 19:34:25 -0500

    modulo de administracion de empleados

--- Controllers/EmpleadoController.cs
+++ Controllers/EmpleadoController.cs
@@ -23,5 +23,27 @@ namespace Thebook.Controllers
             var empleadoDto = await _empleadoService.GetById(id);
             return empleadoDto == null ? NotFound() : Ok(empleadoDto);
         }
+
+        [HttpPost]
+        public async Task<ActionResult<EmpleadoDto>> Add(EmpleadoInsertDto empleadoInsertDto)
+        {
+            var empleadoDto = await _empleadoService.Add(empleadoInsertDto);
+            return empleadoDto == null ? NotFound() : Ok(empleadoDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<EmpleadoDto>> Update(int id, EmpleadoUpdateDto empleadoUpdateDto)
+        {
+            var empleadoDto = await _empleadoService.Update(id, empleadoUpdateDto);
+
+            return empleadoDto == null ? NotFound() : Ok(empleadoDto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<EmpleadoDto>> Delete(int id)
+        {
+            var empleadoDto = await _empleadoService.Delete(id);
+            return empleadoDto == null ? NotFound() : Ok(empleadoDto);
+        }
     }
 }
--- DTOs/EmpleadoGetDto.cs
+++ DTOs/EmpleadoGetDto.cs
@@ -3,8 +3,7 @@
     public class EmpleadoGetDto
     {
         public int IdEmpleado { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string Email { get; set; }        
         public string Rol { get; set; }
     }
 }
