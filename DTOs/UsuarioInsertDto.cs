ff58513605b45dca2aaa3d298374bc44e4e60e83
victor49 <vl491249@gmail.com>
2026-01-27 20:28:25 -0500

    modulo de gestion de libros repository y service

---
+++ DTOs/UsuarioInsertDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Thebook.DTOs
+{
+    public class UsuarioInsertDto
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Identificacion { get; set; }
+        public string Correo { get; set; }
+        public string Celular { get; set; }
+    }
+}
---
+++ Services/IUsuarioService.cs
@@ -0,0 +1,12 @@
+﻿using Thebook.DTOs;
+
+namespace Thebook.Services
+{
+    public interface IUsuarioService
+    {
+        Task<IEnumerable<UsuarioGetDto>> Get();
+        Task<UsuarioGetDto> GetById(int id);
+        Task<UsuarioGetDto> Add(UsuarioInsertDto usuarioInsertDto);
+        Task<UsuarioGetDto> Update(int id, UsuarioUpdateDto usuarioUpdateDto);
+    }
+}
